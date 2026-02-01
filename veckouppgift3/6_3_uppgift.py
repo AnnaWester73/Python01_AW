d005c848f0c088c0b09f90931a3d694dee488292
Anna Wester <annawester73@gmail.com>
2026-02-01 16:52:57 +0100

    Upgifter vecka 3

--- veckouppgift3/6_3_uppgift.py
+++ veckouppgift3/6_3_uppgift.py
@@ -6,17 +6,23 @@
 # 1. Se innehållet i din lista
 # 2. Lägga till nya punkter i din lista
 # 3. Markera som klart
+# 4. Se färdiga saker
+# 5. Lägg tillbaka färdiga saker
 # Välj ett alternativ: 1
 # Din lista är tom
 
+# Skapar tomma listor för saker som ska göras och som är klara
 todo_list = []
+completed_list = []
 
 while True:
     print("-------------------------------------")
     print("1. Se innehåll i din lista")
     print("2. Lägg till nya saker i listan")
     print("3. Markera som klar")
-    print("4. Avsluta")
+    print("4. Se färdiga saker")
+    print("5. Lägg tillbaka färdiga saker")
+    print("6. Avsluta")
     print("-------------------------------------")
 
     val = input("Välj ett alternativ: ")
@@ -25,8 +31,7 @@ while True:
         # not ger sant värde och därför skriv det ut att listan är tom
         if not todo_list:
             print("Din lista är tom")
-
-        # Skriver ut alla saker som lagt till i todo_listan på nya rader
+        # Annars skriver ut vad listan innehåller
         else:
             print("Din lista innehåller: ")
             for task in todo_list:
@@ -49,14 +54,39 @@ while True:
 
             selected_number = int(input("Ange numret: "))
             if 1 <= selected_number <= len(todo_list):
-
                 # pop tar bort sak ur listan istället för remove med hjälp av värde
                 completed_task = todo_list.pop(selected_number)
+                completed_list.append(completed_task)
                 print("Tog nu bort:", completed_task, "i listan!")
             else:
                 print("Ogiltigt val, försök igen.")
 
     elif val == "4":
+        if not completed_list:
+            print("Inga färdiga saker ännu.")
+        else:
+            print("Färdiga saker:")
+            for index, task in enumerate(completed_list):
+                print(index, ".", task)
+
+    elif val == "5":
+        if not completed_list:
+            print("Det finns inga saker att lägga tillbaka.")
+        else:
+            print("Vad vill du lägga tillbala?")
+            for index, task in enumerate(completed_list):
+                print(index, ".", task)
+
+            selected_number = int(input("Ange numret: "))
+            if 0 <= selected_number <= len(completed_list):
+                task = completed_list.pop(selected_number)
+                todo_list.append(task)
+                print("Tillbakalagd i todo-listan:", task)
+            else:
+                print("Ogiltigt val.")
+
+
+    elif val == "6":
         print("Programmet avslutas.")
         break
 
